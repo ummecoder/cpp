6fa855460a384a195624ada81786307c625c4fa8
ummecoder <umesabiha572@gmail.com>
2026-07-05 16:09:58 -0700

    add sub4.cpp

---
+++ sub4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, diff;
+    cout << "Enter 3 numbers: ";
+    cin >> a >> b >> c;
+    diff = a - b - c;
+    cout << "Result: " << diff << endl;
+    return 0;
+}
