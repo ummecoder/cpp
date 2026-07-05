934fddb0136eca404ff68cdb574dd7220ff5053c
ummecoder <umesabiha572@gmail.com>
2026-07-05 12:52:56 -0700

    add basics CPP program

--- ume.cpp
+++ ume.cpp
@@ -1,17 +1,3 @@
 #include <iostream>
 using namespace std;
 int main()
-{
-    int rows;
-    cout << " enter row number:";
-    cin >> rows;
-    for( int i=1;i<=rows;i++)
-    {
-        for( int j =1;j<=i;j++)
-        cout << " * ";
-
-        cout << endl;
-    }
-    
-    return 0;
-}
