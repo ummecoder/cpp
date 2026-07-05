8ba5842cb7f9fe4cbb653b6ea00e6149ea79cc4f
ummecoder <umesabiha572@gmail.com>
2026-07-05 16:28:55 -0700

    add mul3.cpp

---
+++ mul3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b, mul;
+    cout << "Enter 2 decimal numbers: ";
+    cin >> a >> b;
+    mul = a * b;
+    cout << "Result: " << mul << endl;
+    return 0;
+}
