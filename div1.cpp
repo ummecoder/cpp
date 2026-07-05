40043e80b73860c80f4c640c649f36adca4e07ab
ummecoder <umesabiha572@gmail.com>
2026-07-05 16:47:12 -0700

    add div1.cpp

---
+++ div1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b, div;
+    cout << "Enter 2 numbers: ";
+    cin >> a >> b;
+    div = a / b;
+    cout << "Division: " << div << endl;
+    return 0;
+}
