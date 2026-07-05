2146c42b3bae43607c05f3f91b4af3e0e5ab4789
ummecoder <umesabiha572@gmail.com>
2026-07-05 14:16:20 -0700

    add input5.cpp

---
+++ input5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b ;
+    cout << " enter two number : ";
+    cin >> a >> b;
+    cout << " first : " << a << " second: "<< b << endl;
+    return 0;
+}
