49c830aed172a4c765cc3a7a588aa8fc630b4f71
ummecoder <umesabiha572@gmail.com>
2026-07-05 12:56:16 -0700

    add days.cpp

---
+++ days.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << " File:days.cpp "<< endl;
+    cout << " week days "<< endl;
+    cout << " Mon Tue Wed Thurs Fri Satur "<< endl;
+    return 0;
+}
