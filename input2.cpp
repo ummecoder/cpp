ec3904c3506e5a7be65a34ef29809b549e96f68f
ummecoder <umesabiha572@gmail.com>
2026-07-05 13:33:14 -0700

    add input2.cpp

---
+++ input2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout << " enter your name :";
+    getline( cin,name);
+    cout << " hello " << name << endl;
+    return 0;
+}
