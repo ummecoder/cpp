75839468dd5172f8696b4b5f73d4fbdfa7478f4a
ummecoder <umesabiha572@gmail.com>
2026-07-05 16:32:10 -0700

    add mul4.cpp

---
+++ mul4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, mul;
+    cout << "Enter 3 numbers: ";
+    cin >> a >> b >> c;
+    mul = a * b * c;
+    cout << "Result: " << mul << endl;
+    return 0;
+}
