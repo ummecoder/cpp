8f38942ca5d9d1737056865dcb463d13b0c6414c
ummecoder <umesabiha572@gmail.com>
2026-07-05 16:21:21 -0700

    add mul1.cpp

---
+++ mul1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, mul;
+    cout << "Enter 2 numbers: ";
+    cin >> a >> b;
+    mul = a * b;
+    cout << "Multiply: " << mul << endl;
+    return 0;
+}
