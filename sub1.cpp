300752401aad365526837389683b7161ed6d3962
ummecoder <umesabiha572@gmail.com>
2026-07-05 16:00:05 -0700

    add sub1.cpp

---
+++ sub1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, diff;
+    cout << "Enter 2 numbers: ";
+    cin >> a >> b;
+    diff = a - b;
+    cout << "Difference: " << diff << endl;
+    return 0;
+}
