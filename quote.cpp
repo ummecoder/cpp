bbfad436dd4f0e040baa90e3a9b5cfef086774d2
ummecoder <umesabiha572@gmail.com>
2026-07-05 12:44:22 -0700

    add quote.cpp

---
+++ quote.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << " keep coding "<<endl;
+    cout << " keep growing "<<endl;
+    cout << " never give up! "<<endl;
+    return 0;
+}
