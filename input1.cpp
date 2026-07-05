5bf1c66b509f9fccda5bf1842e55372f6c55bf86
ummecoder <umesabiha572@gmail.com>
2026-07-05 13:24:09 -0700

    add input1.cpp

---
+++ input1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << " enter any number :";
+    cin >>num;
+    cout << " you entered :" << num << endl;
+    return 0;
+}
