5fe43ceb8260c58e0917f7a4b50f58d4d58a6d10
ummecoder <umesabiha572@gmail.com>
2026-07-05 15:23:45 -0700

    add Add3.cpp

---
+++ Add3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a,b ,sum;
+    cout << " enter two decimal number : ";
+    cin >> a >> b;
+    sum=a+b;
+    cout << " sum: " << sum << endl;
+    return 0;
+}
