1521f10f9be9312b9eb2b094c12e774f180b7a84
ummecoder <umesabiha572@gmail.com>
2026-07-05 12:29:34 -0700

    add info.cpp

---
+++ info.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << " Name: ume e sabiha " << endl;
+    cout << " City:gujrat " << endl;
+    cout << " subject:C++ programming " << endl;
+    return 0;
+}
