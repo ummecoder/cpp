e131d442809b912bb19a723afd694ebf0952b89b
ummecoder <umesabiha572@gmail.com>
2026-07-05 16:07:03 -0700

    add sub3.cpp

---
+++ sub3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b, diff;
+    cout << "Enter 2 decimal numbers: ";
+    cin >> a >> b;
+    diff = a - b;
+    cout << "Result: " << diff << endl;
+    return 0;
+}
