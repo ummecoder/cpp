9a25866edfad852ecf0240bf430c62d374524ff8
ummecoder <umesabiha572@gmail.com>
2026-07-05 14:03:31 -0700

    add input4.cpp

---
+++ input4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    float marks;
+    cout << " enter your marks : ";
+    cin >> marks;
+    cout << " your marks : " << marks << endl;
+    return 0;
+}
