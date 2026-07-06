46ac5fc674241d1f0fb929f49ca0ba283bf7cb5d
ummecoder <umesabiha572@gmail.com>
2026-07-05 19:28:39 -0700

    add if else program

---
+++ if else4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    if (num > 0) {
+        cout << "Positive number";
+    }
+    else {
+        if (num < 0) {
+            cout << "Negative number";
+        }
+        else {
+            cout << "Zero hai";
+        }
+    }
+    return 0;
+}
