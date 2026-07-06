6a3830859947ba8cd8a3b5aba7a1fbba099230f0
ummecoder <umesabiha572@gmail.com>
2026-07-05 19:44:40 -0700

    add if else if program

---
+++ if else if3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float bmi;
+    cout << "Enter your BMI: ";
+    cin >> bmi;
+    
+    if (bmi < 18.5) {
+        cout << "Underweight";
+    }
+    else if (bmi < 25) {
+        cout << "Normal weight";
+    }
+    else if (bmi < 30) {
+        cout << "Overweight";
+    }
+    else {
+        cout << "Obese";
+    }
+    return 0;
+}
