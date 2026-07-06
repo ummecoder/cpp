353e65811690e0ee0535f5c612c76892eee4d678
ummecoder <umesabiha572@gmail.com>
2026-07-05 20:32:38 -0700

    add increment program

---
+++ incre2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Counting from 1 to 5 using increment:" << endl;
+    
+    for(int i = 1; i <= 5; i++) {
+        cout << "Current value: " << i << endl;
+    }
+    
+    return 0;
+}
