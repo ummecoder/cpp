271c62d9405a814394949cc65eedd3995416a0ed
ummecoder <umesabiha572@gmail.com>
2026-07-05 18:15:16 -0700

    Add multiply.cpp

---
+++ mul6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, result;
+    
+    cout << "Pehla number likho: ";
+    cin >> num1;
+    
+    cout << "Dusra number likho: ";
+    cin >> num2;
+    
+    result = num1 * num2;
+    
+    cout << "Multiply ka result hai: " << result << endl;
+    
+    return 0;
+}
