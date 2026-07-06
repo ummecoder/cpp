b045b74cc4f16f80ec79f91588ff868ebda042b8
ummecoder <umesabiha572@gmail.com>
2026-07-05 21:08:33 -0700

    add while loop program

---
+++ while1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;  // initialization
+    
+    cout << "Numbers from 1 to 10:" << endl;
+    
+    while(i <= 10) {  // condition
+        cout << i << " ";
+        i++;  // increment
+    }
+    
+    return 0;
+}
