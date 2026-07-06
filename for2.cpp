f14a6a2c6260d669a4a161dc990462503e701f68
ummecoder <umesabiha572@gmail.com>
2026-07-05 20:59:33 -0700

    add for loop programs

---
+++ for2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Countdown:" << endl;
+    
+    for(int i = 10; i >= 1; i--) {  // i-- decrements
+        cout << i << endl;
+    }
+    
+    cout << "Happy New Year!" << endl;
+    
+    return 0;
+}
