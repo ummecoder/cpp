492c7517ef7d9d02f733583996c9acebf9443972
ummecoder <umesabiha572@gmail.com>
2026-07-05 20:52:11 -0700

    add decrement program

---
+++ decre4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 10;
+    
+    cout << "Original number: " << num << endl;
+    cout << "Using Pre Decrement: " << --num << endl;  // prints 9
+    cout << "Number after decrement: " << num << endl; // prints 9
+    
+    return 0;
+} 
