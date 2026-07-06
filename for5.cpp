fb536f1855b851c2652b3c7ea1853b0310306c41
ummecoder <umesabiha572@gmail.com>
2026-07-05 21:04:34 -0700

    add for loop program

---
+++ for5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Numbers from 1 to 10:" << endl;
+    
+    for(int i = 1; i <= 10; i++) {  // i++ increments
+        cout << i << " ";
+    }
+    
+    return 0;
+}
